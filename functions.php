30450631f59eb640cab537b6e55078c3b65b886c
lev777 <4439012+lev777@users.noreply.github.com>
2024-04-08 11:17:47 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This is the child theme for Twenty Sixteen theme
+ */
+
+add_action( 'wp_enqueue_scripts', 'twenty_sixteen_enqueue_styles' );
+function twenty_sixteen_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',  get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+}
+
+/*
+ * Override autosuggest max 
+ */
+
+wp_register_script(
+  'fep-script',
+  get_stylesheet_directory_uri() . '/assets/js/script.js',
+  array('jquery'),
+  '1.0.0', 
+  true  
+);
+
+/**
+ * Registers an editor stylesheet for the theme.
+ */
+function wpdocs_theme_add_editor_styles() {
+    add_editor_style( 'custom-editor-style.css' );
+}
+add_action( 'admin_init', 'wpdocs_theme_add_editor_styles' );
